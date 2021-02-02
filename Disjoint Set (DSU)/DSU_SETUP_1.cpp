4282c082693fc01562f1e96b19701ad9214dc3e1
Võ Duy Hùng <76021536+HungVoCs47@users.noreply.github.com>
2021-02-02 20:20:54 +0700

    Add files via upload

---
+++ Disjoint Set (DSU)/DSU_SETUP_1.cpp
@@ -0,0 +1,45 @@
+# include <bits/stdc++.h>
+using namespace std;
+const int MAX=10;
+int parent[MAX];
+void make_set(int n)
+{
+	for(int i=1;i<=n;i++)
+	{
+		parent[i]=i;
+	}
+}
+int FindSet(int u)
+{
+    while(parent[u]!=u) u=parent[u];
+    return u;
+}
+void Union_Set(int u, int v)
+{
+	int pu=FindSet(u);
+	int pv=FindSet(v);
+	parent[pv]=pu;
+}
+int main()
+{
+	ios_base::sync_with_stdio(0); cout.tie(0); cout.tie(0);
+	int n,q;
+	cin>>n>>q;
+	make_set(n);
+	int u,v,t;
+	while(q--)
+	{
+		cin>>t>>u>>v;
+		if(t==1)
+	    {
+	    	Union_Set(u,v);
+		}
+		else if(t==2)
+		{
+			int pu=FindSet(u);
+			int pv=FindSet(v);
+			cout<<(pu==pv ? "Yes\n" : "No\n");
+		}
+	}
+	return 0;
+}
