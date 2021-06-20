97750bf330218aba118a6d99d528a87bbe90bf73
Võ Duy Hùng <76021536+HungVoCs47@users.noreply.github.com>
2021-06-20 15:57:29 +0700

    Create BinarySearch.py

---
+++ Algorithmic Complexity/BinarySearch.py
@@ -0,0 +1,17 @@
+def binarySearch(a, left, right, x):
+    while left <= right:
+        mid = (left + right) // 2
+        if x == a[mid]:
+             return mid
+        elif x < a[mid]:
+            right = mid - 1
+        else:
+            left = mid + 1
+    return -1
+
+
+if __name__ == '__main__':
+    n, x = map(int, input().split())
+    a = list(map(int, input().split()))
+    result = binarySearch(a, 0, n-1, x)
+    print(result)
